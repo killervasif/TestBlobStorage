df45eec8743fb91ec21f63fbe160a34a4a94e5c1
Baba_zd65 <Baba_zd65@STHQ0118-14>
2023-10-09 21:19:15 +0400

    Add project files.

---
+++ TestBlobStorage/Services/BlobStorageManager.cs
@@ -0,0 +1,87 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Sas;
+using Microsoft.Extensions.Options;
+using TestBlobStorage.Utilities;
+
+namespace TestBlobStorage.Services
+{
+    public class BlobStorageManager : IStorageManager
+    {
+        public readonly BlobStorageOptions _storageOptions;
+
+        public BlobStorageManager(IOptions<BlobStorageOptions> options)
+        {
+            _storageOptions = options.Value;
+        }
+
+        public bool DeleteFile(string fileName)
+        {
+            var serviceClient = new BlobServiceClient(_storageOptions.ConnectionString);
+            var contaionerClient = serviceClient.GetBlobContainerClient(_storageOptions.ContainerName);
+            var blobClient = contaionerClient.GetBlobClient(fileName);
+
+            return blobClient.DeleteIfExists().Value;
+        }
+
+        public async Task<bool> DeleteFileAsync(string fileName)
+        {
+            var serviceClient = new BlobServiceClient(_storageOptions.ConnectionString);
+            var contaionerClient = serviceClient.GetBlobContainerClient(_storageOptions.ContainerName);
+            var blobClient = contaionerClient.GetBlobClient(fileName);
+
+            var response = await blobClient.DeleteIfExistsAsync();
+            return response.Value;
+        }
+
+        public string GetSignedUrl(string fileName)
+        {
+            var serviceClient = new BlobServiceClient(_storageOptions.ConnectionString);
+            var contaionerClient = serviceClient.GetBlobContainerClient(_storageOptions.ContainerName);
+            var blobClient = contaionerClient.GetBlobClient(fileName);
+
+            var signedUrl = blobClient.GenerateSasUri(BlobSasPermissions.Read, DateTime.Now.AddHours(2)).AbsoluteUri;
+            
+            return signedUrl;
+        }
+
+        public async Task<string> GetSignedUrlAsync(string fileName)
+        {
+            var serviceClient = new BlobServiceClient(_storageOptions.ConnectionString);
+            var contaionerClient = serviceClient.GetBlobContainerClient(_storageOptions.ContainerName);
+            var blobClient = contaionerClient.GetBlobClient(fileName);
+
+            var signedUrl = blobClient.GenerateSasUri(BlobSasPermissions.Read, DateTime.Now.AddHours(2)).AbsoluteUri;
+
+            return await Task.FromResult(signedUrl);
+        }
+
+        public bool UploadFile(Stream stream, string fileName, string contentType)
+        {
+            var serviceClient = new BlobServiceClient(_storageOptions.ConnectionString);
+            var contaionerClient = serviceClient.GetBlobContainerClient(_storageOptions.ContainerName);
+            var blobClient = contaionerClient.GetBlobClient(fileName);
+
+            blobClient.Upload(stream, new BlobUploadOptions
+            {
+                HttpHeaders = new BlobHttpHeaders { ContentType = contentType }
+            });
+
+            return true;
+        }
+
+        public async Task<bool> UploadFileAsync(Stream stream, string fileName, string contentType)
+        {
+            var serviceClient = new BlobServiceClient(_storageOptions.ConnectionString);
+            var contaionerClient = serviceClient.GetBlobContainerClient(_storageOptions.ContainerName);
+            var blobClient = contaionerClient.GetBlobClient(fileName);
+
+            await blobClient.UploadAsync(stream, new BlobUploadOptions
+            {
+                HttpHeaders = new BlobHttpHeaders { ContentType = contentType }
+            });
+
+            return true;
+        }
+    }
+}
